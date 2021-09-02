21a2fe2436192d81adbe62d127a7039c18828bd7
Gregory Marklew <greg_marklew@hotmail..com>
2021-09-02 15:59:01 +0100

    solution

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI, {
+async function connect() {
+  await mongoose.connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(self => {
-    console.log(`Connected to the database: "${self.connection.name}"`);
-    // Before adding any recipes to the database, let's remove all existing ones
-    return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
+    useUnifiedTopology: true,
+    useFindAndModify : false,
   });
+  console.log("Connected to the database");
+  await Recipe.deleteMany();
+  updateDatabase();
+}
+async function updateDatabase() {
+  try {
+    const createdRecipe = await Recipe.create({
+      title: "Brigadeiro",
+      level: "Easy Peasy",
+      ingredients: ["condensed milk", "powdered chocolate"],
+      cuisine: "Brazilin",
+      dishType: "dessert",
+      duration: "25",
+      creator: "unknown",
+    });
+    await Recipe.insertMany(data);
+    data.forEach((element) => {
+      console.log(element.title);
+    });
+    await Recipe.findOneAndUpdate({ title: "Rigatoni alla Genovese" }, { duration: 100 });
+    await Recipe.deleteOne ({title:"Carrot Cake"})
+  } catch (e) {
+    console.log("Error connecting to the database", e);
+  }
+   finally {
+    mongoose.connection.close();
+  }
+}
+connect();
